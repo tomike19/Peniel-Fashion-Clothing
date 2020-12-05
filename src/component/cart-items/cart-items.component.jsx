9f43727ce58f955399545fc947e56f1a20a1230b
tomike19 <kolajoelizabeth@gmail.com>
2020-12-05 00:16:47 -0800

    adding images to Add to cart

--- src/component/cart-items/cart-items.component.jsx
+++ src/component/cart-items/cart-items.component.jsx
@@ -5,7 +5,7 @@ import './cart-items.styles.scss'
 
 const CartItems = ({item:{imageUrl,price,name,quantity}}) =>(
   <div className='cart-item'>
-    <img src="{imageUrl}" alt="item"/>
+    <img src={imageUrl} alt="item"/>
     <div className='item-details'>
 <span className='name'>{name}</span>
 <span className='price'>{quantity} x ${price}</span>
