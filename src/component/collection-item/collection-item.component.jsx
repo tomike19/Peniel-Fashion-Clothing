0e66a7004b95e468ca22e992a17500b7c67f2d87
tomike19 <kolajoelizabeth@gmail.com>
2020-12-04 23:59:11 -0800

    cart-utils

--- src/component/collection-item/collection-item.component.jsx
+++ src/component/collection-item/collection-item.component.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
 import CustomButton from '../custom-button/custom-button'
 import './collection-item.styles.scss';
+import {addItem} from '../redux/cart/cart-action'
 
 
 
-const CollectionItem = ({id,name, price, imageUrl}) => {
-return(
-<div className='collection-item'>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
 
-    <div className='image'
-    style={{
-      backgroundImage:`url(${imageUrl})`
-    }}/>
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
       <div className='collection-footer'>
-  <span className='name'>{name}</span>
-  <span className='price'>{price}</span>
-  <span className='id'>{id}</span>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
       </div>
-      <CustomButton inverted>Add to Cart</CustomButton>
-      </div>
-     
-)
-  }
-export default CollectionItem;
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
--- src/component/pages/shop/shop.component.jsx
+++ src/component/pages/shop/shop.component.jsx
@@ -3,27 +3,25 @@ import React, { Component } from 'react';
 import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../collection-preview/collection-preview.jsx';
 
-export default class ShopPage extends Component {
-constructor(props) {
-  super(props)
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  this.state = {
-     collections:SHOP_DATA
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
-
-  render() {
-    const{collections} =this.state
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({id,...otherCollectionProps}) => (
-           <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>);
-  }
-
-  }
-
+export default ShopPage;
